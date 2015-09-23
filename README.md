8072cd0e710cf3f8c0a4296453719df74ad9a51d
dubeya <dubeya@192.168.247.1>
2015-09-23 12:44:03 +0530

    TinyURL FirstCommit

---
+++ README.md
@@ -0,0 +1,8 @@
+TINYURL
+==========
+
+This is a simple URL shortner.
+
+Implemented in Node.js, using Redis as the datastore, and Express to handle routing.
+
+
---
+++ app.js
@@ -0,0 +1,90 @@
+/*jslint node: true */
+'use strict';
+
+// Declare variables used
+var app, base_url, bodyParser, client, express, morgan, port, rtg, shortid;
+
+// Define values
+express = require('express');
+app = express();
+port = process.env.PORT || 5000;
+shortid = require('shortid');
+bodyParser = require('body-parser');
+base_url = process.env.BASE_URL || 'http://localhost:5000';
+morgan = require('morgan');
+
+// Set up connection to Redis
+if (process.env.REDISTOGO_URL) {
+    rtg  = require("url").parse(process.env.REDISTOGO_URL);
+    client = require("redis").createClient(rtg.port, rtg.hostname);
+    client.auth(rtg.auth.split(":")[1]);
+} else {
+    client = require('redis').createClient();
+}
+
+// Set up templating
+app.set('views', __dirname + '/views');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+
+// Set URL
+app.set('base_url', base_url);
+
+// Set up logging
+app.use(morgan('combined'));
+
+// Handle POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Define index route
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+// Define submit route
+app.post('/', function (req, res) {
+    // Declare variables
+    var url, id;
+
+    // Get URL
+    url = req.body.url;
+
+    // Create a hashed short version
+    id = shortid.generate();
+
+    // Store them in Redis
+    client.set(id, url, function () {
+        // Display the response
+        res.render('output', { id: id, base_url: base_url });
+    });
+});
+
+// Define link route
+app.route('/:id').all(function (req, res) {
+    // Get ID
+    var id = req.params.id.trim();
+
+    // Look up the URL
+    client.get(id, function (err, reply) {
+        if (!err && reply) {
+            // Redirect user to it
+            res.status(301);
+            res.set('Location', reply);
+            res.send();
+        } else {
+            // Confirm no such link in database
+            res.status(404);
+            res.render('error');
+        }
+    });
+});
+
+// Serve static files
+app.use(express.static(__dirname + '/static'));
+
+// Listen
+app.listen(port);
+console.log('Listening on port ' + port);
